c5c28495ba1f351200221cd98eaa49ed7d49b7bc
brackets020207 <87060254+brackets020207@users.noreply.github.com>
2024-01-21 01:13:59 +1100

    first commit. Added links to pages from index

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -7,4 +8,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def view_page(request, page_name):
+    return render(request, "encyclopedia/entry.html", {
+        "page_name": page_name,
+        "page_content": util.get_entry(page_name)
+    })
+
+    
 
